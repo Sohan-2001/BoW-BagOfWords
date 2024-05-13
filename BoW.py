1d5111c33c9eee8f3cf0fc52847863d4d212654c
Sohan-2001 <112119230+Sohan-2001@users.noreply.github.com>
2024-05-14 04:19:31 +0530

    Update BoW.py

--- BoW.py
+++ BoW.py
@@ -1,5 +1,6 @@
-import numpy as np
-print('BoW - Bag Of Words (To count word frequency in a string)\n\n')
+
+print('\n\nBoW - Bag Of Words (To count word frequency in a string)\n\n')
+
 check=0
 print(':::Enter:::\n')
 temp=''
@@ -11,24 +12,20 @@ while check==0:
         break
  
 print('Here is the list of words and their frequencies in this string of',len(temp),'entries in total\n')
-List=list(temp.split(' '))
-Repeated=[]
-mostCount=0
-mostString=""
-for i in range(0,len(List)):
-    count=0
-    if List[i] not in Repeated and List[i]!='':
-        Repeated.append(List[i])
-        print(List[i]+'--->',end='')
-        for j in range(i+1,len(List)):
-            if List[i]==List[j]:
-                count+=1
-                
-        print(count+1,end='\n')
-        if mostCount<count+1:
-            mostCount=count+1
-            mostString=List[i]
-    
+List=temp.split()
 
-print('\n\nThe most repeated word is {',mostString,'} with its repeatition of {',mostCount,'} times\n\n')
+dictionary={}
+
+for i in List:
+    try:
+        dictionary[i]+=1
+    except:
+        dictionary[i]=1
     
+dictionary=sorted(dictionary.items(), key=lambda x: x[1])
+
+for i,j in dictionary:
+    if i!="":
+        print(i,"-->",j)
+
+print('\n\n')
